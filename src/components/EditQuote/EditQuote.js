051a4abdd5051114babbc618c9cdfdb56782ce09
Irina Isakzhanova <irina.isakjanova@gmail.com>
2021-08-28 12:56:09 +0600

    Created Edit form, added two-way binding

--- src/components/EditQuote/EditQuote.js
+++ src/components/EditQuote/EditQuote.js
@@ -1,9 +1,79 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useHistory} from "react-router-dom";
+
+import Spinner from "../UI/Spinner/Spinner";
+import {categories} from "../../constants";
+
+import "./EditQuote.css";
 
 const EditQuote = () => {
+    const history = useHistory();
+
+    const [quote, setQuote] = useState({
+        category: history.location.state?.category || 'star-wars',
+        author: history.location.state?.author || '',
+        text: history.location.state?.text || ''
+    });
+
+    const [loading, setLoading] = useState(false);
+    const [fieldError, setFieldError] = useState('');
+
+    const handleFieldChange = e => {
+        const {name, value} = e.target;
+        setQuote(prev => ({
+            ...prev,
+            [name]: value,
+        }));
+    };
+
+    const handleFormSubmit = () => {
+
+    }
+
     return (
-        <div>
-            Add Quote
+        <div className="EditQuoteForm">
+            {loading
+                ?
+                <Spinner />
+                :
+                <form onSubmit={handleFormSubmit}>
+                    <label>
+                        <p>Category</p>
+                        <select
+                            name="category"
+                            onChange={handleFieldChange}
+                        >
+                            {categories.map(category => (
+                                <option
+                                    value={category.id}
+                                    key={category.id}
+                                >
+                                    {category.title}
+                                </option>
+                            ))}
+                        </select>
+                    </label>
+                    <label>
+                        <p>Title</p>
+                        <input
+                            type="text"
+                            name="author"
+                            value={quote.author}
+                            onChange={handleFieldChange}
+                        />
+                    </label>
+                    <label>
+                        <p>Description</p>
+                        <textarea
+                            name="text"
+                            value={quote.text}
+                            onChange={handleFieldChange}
+                        />
+                    </label>
+                    <button type="submit">Save</button>
+                </form>
+            }
+            <p className="FieldError">{fieldError}</p>
         </div>
     );
 };
--- src/constants.js
+++ src/constants.js
@@ -4,3 +4,11 @@ export const navItems = [
     {name: 'Quotes', id: nanoid(), route: '/quotes'},
     {name: 'Submit new quote', id: nanoid(), route: '/add-quote'},
 ];
+
+export const categories = [
+    {title: 'Star Wars', id: 'star-wars'},
+    {title: 'Famous people', id: 'famous-people'},
+    {title: 'Saying', id: 'saying'},
+    {title: 'Humour', id: 'humor'},
+    {title: 'Motivational', id: 'motivational'},
+];
