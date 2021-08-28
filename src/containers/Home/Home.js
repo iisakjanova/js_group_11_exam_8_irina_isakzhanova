2d70e8965b60107055d87aad9db1adde6ca41122
Irina Isakzhanova <irina.isakjanova@gmail.com>
2021-08-28 14:06:47 +0600

    Realized navigation between categories

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Categories from "../../components/Categories/Categories";
 
 const Home = () => {
     return (
         <div>
-            Home page
+            <Categories />
         </div>
     );
 };
